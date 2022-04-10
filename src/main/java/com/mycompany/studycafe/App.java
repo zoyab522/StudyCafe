eca68b036e6e1959839088d4395dd3c3cd37d76c
Zoya <zoya.artsy@gmail.com>
2022-04-10 14:26:02 -0400

    created tab

--- src/main/java/com/mycompany/studycafe/App.java
+++ src/main/java/com/mycompany/studycafe/App.java
@@ -3,6 +3,7 @@ package com.mycompany.studycafe;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,13 +15,15 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        TabPane tabPane = new TabPane(); // create a TabPane so we can add tabs
+        FirstTab firstTab = new FirstTab("Placeholder Name"); // Our first tab
+        tabPane.getTabs().addAll(firstTab);
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var scene = new Scene(tabPane, 640, 480);
+        scene.getStylesheets().add("application.css");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mycompany/studycafe/FirstTab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.studycafe;
+
+import javafx.scene.control.Tab;
+
+/**
+ *
+ * @author zoya
+ */
+public class FirstTab extends Tab{
+
+    public FirstTab(String string) {
+        super(string);
+    }
+    
+    
+}
