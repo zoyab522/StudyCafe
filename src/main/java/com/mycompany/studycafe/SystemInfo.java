4a1e5c1b4349e038de622723b11575dc1e6afff8
anikas1 <44713889+anikas1@users.noreply.github.com>
2022-04-11 07:37:53 -0400

    Update SystemInfo.java

--- src/main/java/com/mycompany/studycafe/SystemInfo.java
+++ src/main/java/com/mycompany/studycafe/SystemInfo.java
@@ -1,11 +1,19 @@
 package com.mycompany.studycafe;
 
 public class SystemInfo {
-
+    
+    /**
+     * 
+     * @return 
+     */
     public static String javaVersion() {
         return System.getProperty("java.version");
     }
-
+    
+    /**
+     * 
+     * @return 
+     */
     public static String javafxVersion() {
         return System.getProperty("javafx.version");
     }
